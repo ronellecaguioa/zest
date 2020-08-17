0cf03ef6ec9a3ec3203e74994187a267b4a53e85
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-08-17 11:06:27 -0700

    Create hello world app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './stylesheets/index.scss';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
