abd30d07ed3b2b531753464451bb3353e37fa8f5
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-08-17 17:07:16 -0700

    Add comment

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -5,29 +5,30 @@ export interface CurrentProps {
   current: Data;
 }
 
-const Current: React.FC<CurrentProps> = ({ current }) => {
-  return (
-    <div className="current">
-      <h3>LAST</h3>
-      <hr />
-      <div>
-        {current ? (
-          <>
-            <div>
-              <span>Amount: </span>
-              <span>${current.amount}</span>
-            </div>
-            <div>
-              <span>Time: </span>
-              <span>{current.timestamp.toLocaleTimeString()}</span>
-            </div>
-          </>
-        ) : (
-          <div>Loading</div>
-        )}
-      </div>
+/**
+ * Presentation component for display the latest data point
+ */
+const Current: React.FC<CurrentProps> = ({ current }) => (
+  <div className="current">
+    <h3>LAST</h3>
+    <hr />
+    <div>
+      {current ? (
+        <>
+          <div>
+            <span>Amount: </span>
+            <span>${current.amount}</span>
+          </div>
+          <div>
+            <span>Time: </span>
+            <span>{current.timestamp.toLocaleTimeString()}</span>
+          </div>
+        </>
+      ) : (
+        <div>Loading</div>
+      )}
     </div>
-  );
-};
+  </div>
+);
 
 export default Current;
